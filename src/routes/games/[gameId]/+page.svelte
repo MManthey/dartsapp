7745df7a5eafc51231fe84ef83833fa607ab29e3
Marten Manthey <martenmanthey1995@gmail.com>
2023-11-09 15:28:53 +0100

    cam aspect ration 4/3

--- src/routes/games/[gameId]/+page.svelte
+++ src/routes/games/[gameId]/+page.svelte
@@ -77,8 +77,8 @@
 			if (!camOn) {
 				const camStream = await navigator.mediaDevices.getUserMedia({ 
 					video: { 
-						facingMode: "environment", // back facing cam
-						aspectRatio: 1 // Square aspect ratio
+						facingMode: "environment",
+						aspectRatio: 4 / 3
 					} 
 				});
 				camStream.getVideoTracks().forEach((track) => {
